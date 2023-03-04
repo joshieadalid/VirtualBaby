d629b14cd8636be9c096eb1c83286f0c29b328cd
Josué Adalid <josueadalidjb@gmail.com>
2023-03-04 14:55:18 -0600

    Método SQL getUser

--- src/main/java/com/virtualbaby/connection/MySQL.java
+++ src/main/java/com/virtualbaby/connection/MySQL.java
@@ -1,8 +1,9 @@
 package com.virtualbaby.connection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import com.virtualbaby.entities.Usuario;
+
+import java.sql.*;
+import java.util.ArrayList;
 
 public class MySQL {
     private static MySQL instance = null;
@@ -37,6 +38,30 @@ public class MySQL {
     private void loadConnection() throws SQLException {
         connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/Virtual_Baby","root","root");
         System.out.println("Conexión establecida");
+    }
+
+    ArrayList<Usuario> getUser(String user, String password) throws SQLException {
+        String query = "SELECT * FROM Usuario WHERE idUsuario=? AND password=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, user);
+        preparedStatement.setString(2,password);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        ArrayList<Usuario> users = new ArrayList<>();
+        while (resultSet.next()) {
+            Usuario usuario = new Usuario();
+            usuario.setIdUsuario(resultSet.getString(1));
+            usuario.setNombreUsuario(resultSet.getString(2));
+            usuario.setAp_paterno(resultSet.getString(3));
+            usuario.setAp_materno(resultSet.getString(4));
+            usuario.setEmail(resultSet.getString(5));
+            usuario.setTelefono(resultSet.getString(6));
+            usuario.setPassword(resultSet.getString(7));
+            usuario.setTipo(resultSet.getString(8));
+            users.add(usuario);
+        }
+        preparedStatement.close();
+        return users;
     }
 
     public static void main(String[] args)
---
+++ src/main/java/com/virtualbaby/entities/Usuario.java
@@ -0,0 +1,76 @@
+package com.virtualbaby.entities;
+
+public class Usuario {
+    private String idUsuario;
+    private String nombreUsuario;
+    private String ap_paterno;
+    private String ap_materno;
+    private String email;
+    private String telefono;
+    private String password;
+    private String tipo;
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getAp_paterno() {
+        return ap_paterno;
+    }
+
+    public void setAp_paterno(String ap_paterno) {
+        this.ap_paterno = ap_paterno;
+    }
+
+    public String getAp_materno() {
+        return ap_materno;
+    }
+
+    public void setAp_materno(String ap_materno) {
+        this.ap_materno = ap_materno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
