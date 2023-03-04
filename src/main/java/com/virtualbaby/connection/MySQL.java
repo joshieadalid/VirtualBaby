b4a7c3b7c388e775bf25d50bffa1eed40d5012d1
Josué Adalid <josueadalidjb@gmail.com>
2023-03-04 15:01:22 -0600

    Método getUser() corregido

--- src/main/java/com/virtualbaby/connection/MySQL.java
+++ src/main/java/com/virtualbaby/connection/MySQL.java
@@ -8,7 +8,8 @@ import java.util.ArrayList;
 public class MySQL {
     private static MySQL instance = null;
     Connection connection;
-    public MySQL(){
+
+    public MySQL() {
         try {
             loadDriver();
         } catch (ClassNotFoundException e) {
@@ -21,51 +22,14 @@ public class MySQL {
         }
     }
 
-    public static MySQL getInstance(){
-        if (instance == null){
+    public static MySQL getInstance() {
+        if (instance == null) {
             instance = new MySQL();
         }
         return instance;
     }
 
-    void closeConnection() throws SQLException {
-        connection.close();
-    }
-    private void loadDriver() throws ClassNotFoundException {
-        Class.forName("org.mariadb.jdbc.Driver");
-        System.out.println("Clase cargada");
-    }
-    private void loadConnection() throws SQLException {
-        connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/Virtual_Baby","root","root");
-        System.out.println("Conexión establecida");
-    }
-
-    ArrayList<Usuario> getUser(String user, String password) throws SQLException {
-        String query = "SELECT * FROM Usuario WHERE idUsuario=? AND password=?";
-        PreparedStatement preparedStatement = connection.prepareStatement(query);
-        preparedStatement.setString(1, user);
-        preparedStatement.setString(2,password);
-        ResultSet resultSet = preparedStatement.executeQuery();
-
-        ArrayList<Usuario> users = new ArrayList<>();
-        while (resultSet.next()) {
-            Usuario usuario = new Usuario();
-            usuario.setIdUsuario(resultSet.getString(1));
-            usuario.setNombreUsuario(resultSet.getString(2));
-            usuario.setAp_paterno(resultSet.getString(3));
-            usuario.setAp_materno(resultSet.getString(4));
-            usuario.setEmail(resultSet.getString(5));
-            usuario.setTelefono(resultSet.getString(6));
-            usuario.setPassword(resultSet.getString(7));
-            usuario.setTipo(resultSet.getString(8));
-            users.add(usuario);
-        }
-        preparedStatement.close();
-        return users;
-    }
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         try {
             MySQL.getInstance().loadDriver();
         } catch (ClassNotFoundException e) {
@@ -76,5 +40,40 @@ public class MySQL {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    void closeConnection() throws SQLException {
+        connection.close();
+    }
+
+    private void loadDriver() throws ClassNotFoundException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        System.out.println("Clase cargada");
+    }
+
+    private void loadConnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/Virtual_Baby", "root", "root");
+        System.out.println("Conexión establecida");
+    }
+
+    Usuario getUser(String user, String password) throws SQLException {
+        String query = "SELECT * FROM Usuario WHERE idUsuario=? AND password=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, user);
+        preparedStatement.setString(2, password);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        Usuario usuario = new Usuario();
+        usuario.setIdUsuario(resultSet.getString(1));
+        usuario.setNombreUsuario(resultSet.getString(2));
+        usuario.setAp_paterno(resultSet.getString(3));
+        usuario.setAp_materno(resultSet.getString(4));
+        usuario.setEmail(resultSet.getString(5));
+        usuario.setTelefono(resultSet.getString(6));
+        usuario.setPassword(resultSet.getString(7));
+        usuario.setTipo(resultSet.getString(8));
+
+        preparedStatement.close();
+        return usuario;
     }
 }
